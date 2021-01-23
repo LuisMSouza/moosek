d4d497085ef83ad829c944c9afe5a7c35f86dc26
LuisMSouza <l.miguel-souza@hotmail.com>
2021-01-23 20:03:27 -0300

    commit changes

---
+++ src/commands/join.js
@@ -0,0 +1,33 @@
+////////////////// IMPORTS //////////////////////
+const sendError = require('../utils/error.js');
+const { execute } = require('./play.js');
+
+////////////////// SOURCE CODE //////////////////
+module.exports = {
+    name: "entrar",
+    description: "Para o bot entrar do chat",
+    usage: [process.env.PREFIX_KEY + 'entrar'],
+    aliases: ['e', 'join'],
+
+    async execute(client, message, args) {
+        const voiceChannel = message.member.voice.channel;
+        const serverQueue = client.queue.get(message.guild.id);
+
+        if (!voiceChannel) return sendError("Você precisa estar em um canal de voz para iniciar uma música!", message.channel).then(m => m.delete({ timeout: 10000 }));
+        if (serverQueue) return;
+        const bot = message.guild.members.cache.get(client.user.id);
+        if (bot.voice.channel) {
+            return message.channel.send({
+                embed: {
+                    description: "❌ **O bot já está sendo utilizado!**"
+                }
+            }).then(m2 => m2.delete({ timeout: 10000 }))
+        }
+        await voiceChannel.join();
+        message.channel.send({
+            embed: {
+                description: `Me juntei ao canal **${message.member.voice.channel.name}**`
+            }
+        });
+    }
+}
---
+++ src/commands/leave.js
@@ -0,0 +1,33 @@
+////////////////// IMPORTS //////////////////////
+const sendError = require('../utils/error.js');
+const { execute } = require('./play.js');
+
+////////////////// SOURCE CODE //////////////////
+module.exports = {
+    name: "sair",
+    description: "Para o bot sair no chat",
+    usage: [process.env.PREFIX_KEY + 'sair'],
+    aliases: ['lv', 'leave'],
+
+    async execute(client, message, args) {
+        const voiceChannel = message.member.voice.channel;
+        const serverQueue = client.queue.get(message.guild.id);
+
+        if (!voiceChannel) return sendError("Você precisa estar em um canal de voz para iniciar uma música!", message.channel).then(m => m.delete({ timeout: 10000 }));
+        if (serverQueue) return;
+        const bot = message.guild.members.cache.get(client.user.id);
+        if (bot.voice.channel != message.member.voice.channel) {
+            return message.channel.send({
+                embed: {
+                    description: "❌ **O bot já está sendo utilizado!**"
+                }
+            }).then(m2 => m2.delete({ timeout: 10000 }))
+        }
+        await voiceChannel.leave();
+        message.channel.send({
+            embed: {
+                description: `**Saí do canal ;)**`
+            }
+        });
+    }
+}
