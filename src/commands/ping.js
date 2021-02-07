2d3b444a8e389f5dcef8f83de33ba812fde25fbe
LuisMSouza <l.miguel-souza@hotmail.com>
2021-02-06 23:15:46 -0300

    commit updates

---
+++ src/commands/ping.js
@@ -0,0 +1,20 @@
+/////////////////////// IMPORTS ///////////////////////////
+
+/////////////////////// SOURCE CODE ///////////////////////////
+module.exports = {
+    name: "ping",
+    description: "Mostra o ping da API",
+    usage: [process.env.PREFIX_KEY + 'ping'],
+    category: 'user',
+    timeout: 5000,
+    aliases: [],
+
+    async execute(client, message, args) {
+        let ping = Math.round(message.client.ws.ping);
+        message.channel.send(({
+            embed: {
+                description: `**${ping} ms**`
+            }
+        }))
+    }
+}
