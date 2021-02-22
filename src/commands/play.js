79aa61bedd9f0ea797af8f9898d06bb65eb9541d
LuisMSouza <l.miguel-souza@hotmail.com>
2021-02-22 00:28:18 -0300

    commit updates

--- src/commands/play.js
+++ src/commands/play.js
@@ -124,55 +124,61 @@ module.exports = {
                     return undefined;
                 })
             } catch (err) {
-                console.log(err);
+                if (err.name === "Cannot read property 'title' of undefined") return console.log(`Erro: ${err.name}`);
+                return console.log(err.name);
             }
         }
 
         async function handleVideo(video, message, channel, playlist = false) {
-            const serverQueue = message.client.queue.get(message.guild.id);
+            try {
+                const serverQueue = message.client.queue.get(message.guild.id);
 
-            const song = {
-                id: video.id,
-                title: video.title ? video.title : (await ytdl.getInfo(video.shortUrl)).videoDetails.media.song,
-                url: video.shortUrl,
-                thumbnail: video.thumbnails[0].url,
-                duration: video.duration,
-                isLive: video.isLive,
-            }
-
-            if (!serverQueue) {
-                const queueConstruct = {
-                    textChannel: message.channel,
-                    voiceChannel: channel,
-                    connection: null,
-                    songs: [],
-                    volume: 5,
-                    playing: true,
-                    loop: false
-                };
-                message.client.queue.set(message.guild.id, queueConstruct);
-                queueConstruct.songs.push(song);
-
-                try {
-                    var connection = await channel.join();
-                    queueConstruct.connection = connection;
-                    play(message.guild, queueConstruct.songs[0]);
-                } catch (error) {
-                    console.error(`Eu não consegui entrar no canal: ${error}`);
-                    message.client.queue.delete(message.guild.id);
-                    return sendError(`Eu não consegui entrar no canal: ${error}`, message.channel);
-
+                const song = {
+                    id: video.id,
+                    title: video.title ? video.title : (await ytdl.getInfo(video.shortUrl)).videoDetails.media.song,
+                    url: video.shortUrl,
+                    thumbnail: video.thumbnails[0].url,
+                    duration: video.duration,
+                    isLive: video.isLive,
                 }
-            } else {
-                serverQueue.songs.push(song);
-                if (playlist) return;
-                let thing = new MessageEmbed()
-                    .setTitle(`Música adicionada à fila`)
-                    .setThumbnail(song.img)
-                    .setDescription(`**__${song.title}__** adicionado à fila`)
-                return message.channel.send(thing);
+
+                if (!serverQueue) {
+                    const queueConstruct = {
+                        textChannel: message.channel,
+                        voiceChannel: channel,
+                        connection: null,
+                        songs: [],
+                        volume: 5,
+                        playing: true,
+                        loop: false
+                    };
+                    message.client.queue.set(message.guild.id, queueConstruct);
+                    queueConstruct.songs.push(song);
+
+                    try {
+                        var connection = await channel.join();
+                        queueConstruct.connection = connection;
+                        play(message.guild, queueConstruct.songs[0]);
+                    } catch (error) {
+                        console.error(`Eu não consegui entrar no canal: ${error}`);
+                        message.client.queue.delete(message.guild.id);
+                        return sendError(`Eu não consegui entrar no canal: ${error}`, message.channel);
+
+                    }
+                } else {
+                    serverQueue.songs.push(song);
+                    if (playlist) return;
+                    let thing = new MessageEmbed()
+                        .setTitle(`Música adicionada à fila`)
+                        .setThumbnail(song.img)
+                        .setDescription(`**__${song.title}__** adicionado à fila`)
+                    return message.channel.send(thing);
+                }
+                return;
+            } catch (e) {
+                if (err.name === "Cannot read property 'title' of undefined") return console.log(`Erro: ${err.name}`);
+                return console.log(err.name);
             }
-            return;
         }
 
         async function play(guild, song) {
