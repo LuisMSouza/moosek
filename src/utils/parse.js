d7e5d03b3ed4fb0a1cdd4a63c4264a0953bdf6f8
LuisMSouza <l.miguel-souza@hotmail.com>
2021-03-28 23:42:18 -0300

    commit updates

--- src/utils/parse.js
+++ src/utils/parse.js
@@ -12,7 +12,7 @@ module.exports = {
     },
     async parseError(channel) {
         let embed = new MessageEmbed()
-            .setColor("#701AAB")
+            .setColor("RED")
             .setDescription("```fix\n" + `${process.env.PARSE_ERROR}` + "\n```")
 
         await channel.send(embed)
