81fc2da831df33736bd34bea14a34bb42a134ab0
LuisMSouza <l.miguel-souza@hotmail.com>
2021-04-06 00:14:48 -0300

    commit updates

--- src/utils/botUtils.js
+++ src/utils/botUtils.js
@@ -1,7 +1,7 @@
 exports.QUEUE_LIMIT = 100;
 exports.STAY_TIME = 300;
 exports.CEO_ID = 672129775829254164;
-exports.CLIENT_VERSION = "1.7.0";
+exports.CLIENT_VERSION = "1.8.0";
 exports.PB_USER = process.env.PB_USER;
 exports.PB_PASS = process.env.PB_PASS;
 exports.FUNCTION_KEY = process.env.FC_KEY;
