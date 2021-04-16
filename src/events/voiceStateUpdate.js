501b549606fe60a1250b34cd6d361a4c29635347
LuisMSouza <l.miguel-souza@hotmail.com>
2021-04-16 01:08:11 -0300

    commit updates

--- src/events/voiceStateUpdate.js
+++ src/events/voiceStateUpdate.js
@@ -5,6 +5,7 @@ const guildData = require('../models/guildData.js');
 module.exports = async (client, oldState, newState, message, member) => {
     if (newState.member.id === client.user.id && !newState.connection) {
         const serverQueue = client.queue.get(newState.guild.id);
+        client.radio.delete(newState.guild.id);
         client.queue.delete(newState.guild.id);
         await guildData.findOneAndUpdate({ guildID: newState.guild.id }, { $set: { aleatory_mode: false } }, { new: true });
         serverQueue.looping = false
