f5fdf4264f4bc08ff1b23d4f95d22f44e5c07421
LuisMSouza <l.miguel-souza@hotmail.com>
2021-04-18 22:18:44 -0300

    commit updates

--- src/models/addUrl.js
+++
@@ -1,7 +0,0 @@
-/////////////////////// IMPORTS //////////////////////////
-const userData = require('./userData.js');
-
-/////////////////////// SOURCE CODE ///////////////////////////
-module.exports = async (client, member, songUrl) => {
-    await userData.findOneAndUpdate({ userID: member }, { $set: { list: songUrl++ } }, { new: true });
-}
