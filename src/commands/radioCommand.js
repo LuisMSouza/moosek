5cfead07d70d29ef35f8e5724936de895721d637
LuisMSouza <l.miguel-souza@hotmail.com>
2021-05-29 08:34:43 -0300

    commit updddadtes

--- src/commands/radioCommand.js
+++ src/commands/radioCommand.js
@@ -123,7 +123,7 @@ module.exports = {
                                 //await reaction.users.remove(user);
                                 return;
                             }
-                            if (radioListenConstruct.channel.id !== message.member.channel.id) {
+                            if (radioListenConstruct.channel.id !== message.member.voice.channel.id) {
                                 message.channel.send({
                                     embed: {
                                         color: "#701AAB",
