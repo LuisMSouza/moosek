7724eff50fe63eea541189bb7f2bbbf7088e2de4
LuisMSouza <l.miguel-souza@hotmail.com>
2021-05-31 23:18:59 -0300

    c test

--- src/structures/strDeezerHandle.js
+++ src/structures/strDeezerHandle.js
@@ -5,7 +5,7 @@ const dzr = new Deezer();
 /////////////////////// SOURCE CODE ///////////////////////////
 module.exports = {
     async deezerHandler(client, message, search, cth, voiceChannel) {
-        const regEx
+        //const regEx
         if (cth.includes("/track/")) {
             dzr.track(``);
         } else if (cth.includes("/playlist/")) {
