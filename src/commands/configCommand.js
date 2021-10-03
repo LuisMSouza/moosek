96b1d7c249b6294935eb09f46fc9e6d380682d41
LuisMSouza <l.miguel-souza@hotmail.com>
2021-10-03 00:06:29 -0300

    commit updt

--- src/commands/configCommand.js
+++ src/commands/configCommand.js
@@ -29,39 +29,38 @@ module.exports = {
                 .addField(`> Prefixo`, `Prefixo atual do servidor: ` + "`" + `${pref}` + "`", true)
                 .addField("> Como alterar?", `Basta digitar ` + "`" + `${pref}config prefix` + "` ou clicar no botão abaixo")
                 .setFooter(client.user.username, client.user.displayAvatarURL())
-            /*
-            const btn = new MessageButton()
-                .setCustomId('change_prefix')
-                .setLabel('ALTERAR PREFIXO')
-                .setStyle('PRIMARY')
-*/
+
             const embd = new MessageEmbed()
                 .setDescription("```fix\nDigite o novo prefixo\n```")
 
-            const btnMsg = await message.channel.send(emb);
-            btnMsg.react("🆕")
-            const filter = message.author.id != client.user.id
-            const collector = btnMsg.createReactionCollector((reaction, user) => ["🆕"].includes(reaction.emoji.name) && user != user.bot);
-            collector.on("collect", async (reaction, user) => {
-                switch (reaction.emoji.name) {
-                    case "🆕":
-                        btnMsg.reactions.removeAll()
-                        btnMsg.edit(embd);
-                        const filter2 = m => m.author.id === message.author.id;
-                        message.channel.awaitMessages(filter2, { max: 1, time: 300000, errors: ['time'] })
-                            .then(async collected => {
-                                if (collected.first().content.length >= 5) return sendError("Esse prefixo é muito longo!", message.channel);
-                                collected.first().content.toLowerCase();
-                                await guildData.findOneAndUpdate({ guildID: message.guild.id }, { $set: { guildPrefix: collected.first().content.toLowerCase() } }, { new: true });
-                                btnMsg.delete(btnMsg);
-                                message.channel.send({
-                                    embed: {
-                                        description: "Prefixo alterado para: `" + `${collected.first().content.toLowerCase()}` + "`"
-                                    }
-                                })
-                            }).catch(collected => message.channel.send("Tempo de resposta esgotado"))
-                        return;
-                        break
+            const btnMsg = await message.channel.send(emb).then(async embed => {
+                try {
+                    embed.react("🆕")
+                    const collector = embed.createReactionCollector((reaction, user) => ["🆕"].includes(reaction.emoji.name) && user != user.bot);
+                    collector.on("collect", async (reaction, user) => {
+                        switch (reaction.emoji.name) {
+                            case "🆕":
+                                embed.reactions.removeAll()
+                                embed.edit(embd);
+                                const filter2 = m => m.author.id === message.author.id;
+                                message.channel.awaitMessages(filter2, { max: 1, time: 300000, errors: ['time'] })
+                                    .then(async collected => {
+                                        if (collected.first().content.length >= 5) return sendError("Esse prefixo é muito longo!", message.channel);
+                                        collected.first().content.toLowerCase();
+                                        await guildData.findOneAndUpdate({ guildID: message.guild.id }, { $set: { guildPrefix: collected.first().content.toLowerCase() } }, { new: true });
+                                        embed.delete(embed);
+                                        message.channel.send({
+                                            embed: {
+                                                description: "Prefixo alterado para: `" + `${collected.first().content.toLowerCase()}` + "`"
+                                            }
+                                        })
+                                    }).catch(collected => message.channel.send("Tempo de resposta esgotado"))
+                                return;
+                                break
+                        }
+                    })
+                } catch (e) {
+                    console.log(e);
                 }
             })
         } else {
