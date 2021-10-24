9c681f4063d2eac64164f823b978deeed209b39d
LuisMSouza <l.miguel-souza@hotmail.com>
2021-10-24 18:46:29 -0300

    neww command

---
+++ src/commands/annouceCommand.js
@@ -0,0 +1,21 @@
+/////////////////////// IMPORTS //////////////////////////
+const sendError = require('../utils/error.js');
+const { CEO_ID } = require('../utils/botUtils.js');
+const guildData = require('../models/guildData.js');
+
+/////////////////////// SOURCE CODE //////////////////////////
+module.exports = {
+    name: "announce",
+    description: "Faz um anúncio a todos os servidores em que o bot está",
+    usage: [process.env.PREFIX_KEY + 'announce'],
+    category: 'ceo',
+    timeout: 7000,
+    aliases: ['anc', 'msgall', 'msg'],
+
+    async execute(client, message, args) {
+        if (message.author.id != CEO_ID) return;
+
+        const data = await guildData.find({});
+        console.log(data);
+    }
+}
