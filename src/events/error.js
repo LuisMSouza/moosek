74b863f3ac9570b75ece09760251d9163a2284fc
LuisMSouza <l.miguel-souza@hotmail.com>
2021-10-24 20:25:38 -0300

    updates

--- src/events/error.js
+++ src/events/error.js
@@ -9,7 +9,7 @@ module.exports = async (client, error) => {
             .setDescription(`**${error}**`)
             .setTimestamp(client.user.username, client.user.displayAvatarURL())
 
-        await client.guilds.cache.get("731542666277290016").channels.cache.get("807738719556993064").send(embed);
+        await client.guilds.cache.get("731542666277290016").channels.cache.get("807738719556993064").send({ embeds: [embed] });
         return;
     } catch (e) {
         console.log(e);
