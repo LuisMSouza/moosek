2ddd9e5889bd8454e67e02523f5712b17927b91c
LuisMSouza <l.miguel-souza@hotmail.com>
2021-10-24 18:35:17 -0300

    commit updates

--- src/events/voiceStateUpdate.js
+++ src/events/voiceStateUpdate.js
@@ -1,3 +1,14 @@
-module.exports = async function (oldState, newState) {
-    
+module.exports = async function (client, oldState, newState) {
+    const serverQueue = newState.client.queue.get(newState.guild.id);
+    if (newState.channelId && newState.channel.type === 'GUILD_STAGE_VOICE' && newState.guild.me.voice.suppress) {
+        try {
+            await newState.guild.me.voice.setSuppressed(false);
+        } catch (e) {
+            return console.log(e);
+        }
+    }
+    if (newState.member.id === bot.user.id && !newState.connection) {
+        if (serverQueue) return;
+        bot.queue.delete(newState.guild.id);
+    }
 }
