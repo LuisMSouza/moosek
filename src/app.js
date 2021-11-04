6c79cc8bc5aecbba81724682d0c65e1b4f9d276b
LuisMSouza <l.miguel-souza@hotmail.com>
2021-11-03 23:43:41 -0300

    s

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ const cmnds = []
 const commands = fs.readdirSync(`./src/commands`).filter(file => file.endsWith(".js"));
 for (const file of commands) {
     const cmd = require(`./commands/${file}`);
-    if (cmd.category != 'ceo') cmnds.push(cmd.toJSON());
+    if (cmd.category != 'ceo') cmnds.push(cmd);
     client.commands.set(cmd.name, cmd);
     console.log("Carregando comando: " + cmd.name)
 }
