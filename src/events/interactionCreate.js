f07de79761878065f6c268f29ce13994297498ee
LuisMSouza <l.miguel-souza@hotmail.com>
2021-11-04 01:59:14 -0300

    s

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -2,21 +2,23 @@ module.exports = async function (client, interaction) {
     if (!interaction.channelId || !interaction.guildId) return;;
     if (!interaction.isCommand()) return;
     if (interaction.isCommand()) {
-        await interaction.deferReply();
         const command = client.slashCommands.get(interaction.commandName);
         if (command) {
             if (command.options[0].name != "none") {
                 if (command.options[0].required) {
                     const value = interaction.options.getString(command.options[0].name);
                     const args = value.split(/ +/g);
-                    return command.execute(client, interaction, args);
+                    await command.execute(client, interaction, args);
+                    return interaction.deferReply();
                 } else {
                     const args = []
-                    return command.execute(client, interaction, args);
+                    await command.execute(client, interaction, args);
+                    return interaction.deferReply();
                 }
             }
             const args = []
-            return command.execute(client, interaction, args);
+            await command.execute(client, interaction, args);
+            return interaction.deferReply();
         }
     }
 }
