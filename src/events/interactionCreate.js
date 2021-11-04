ff403dde59cc443c694b0bbd96aabd96393b1fb8
LuisMSouza <l.miguel-souza@hotmail.com>
2021-11-04 00:18:28 -0300

    s

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -3,7 +3,7 @@ module.exports = async function (client, interaction) {
     if (interaction.isCommand()) {
         const command = client.slashCommands.get(interaction.commandName);
         if (command) {
-            const value = interaction.options.name
+            const value = interaction.options.getString(command.options[0].name);
             return command.execute(client, interaction, value);
         }
     }
