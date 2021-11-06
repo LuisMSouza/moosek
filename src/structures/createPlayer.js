f0290068fae5533cb56ac8d6762737f40966eae7
LuisMSouza <l.miguel-souza@hotmail.com>
2021-11-06 01:55:27 -0300

    s

--- src/structures/createPlayer.js
+++ src/structures/createPlayer.js
@@ -365,19 +365,21 @@ module.exports.play = async (client, message, song) => {
                     }
                     break;
             }
-            serverQueue.connection.on(AudioPlayerStatus.Idle, async () => {
-                await playingMessage.edit({ embeds: [embedMusic], components: [] });
-                if (serverQueue.looping) {
-                    let lastSong = serverQueue.songs.shift();
-                    serverQueue.songs.push(lastSong);
-                    return module.exports.play(client, message, serverQueue.songs[0]);
-                } if (serverQueue.nigthCore) {
+            serverQueue.connection.on("stateChange", async () => {
+                if (AudioPlayerStatus.Idle) {
+                    await playingMessage.edit({ embeds: [embedMusic], components: [] });
+                    if (serverQueue.looping) {
+                        let lastSong = serverQueue.songs.shift();
+                        serverQueue.songs.push(lastSong);
+                        return module.exports.play(client, message, serverQueue.songs[0]);
+                    } if (serverQueue.nigthCore) {
+                        if (!serverQueue.songLooping) await serverQueue.songs.shift();
+                        var random = Math.floor(Math.random() * (serverQueue.songs.length));
+                        return module.exports.play(client, message, serverQueue.songs[random]);
+                    }
                     if (!serverQueue.songLooping) await serverQueue.songs.shift();
-                    var random = Math.floor(Math.random() * (serverQueue.songs.length));
-                    return module.exports.play(client, message, serverQueue.songs[random]);
+                    return module.exports.play(client, message, serverQueue.songs[0]);
                 }
-                if (!serverQueue.songLooping) await serverQueue.songs.shift();
-                return module.exports.play(client, message, serverQueue.songs[0]);
             })
             stream.stream.on(AudioPlayerStatus.Idle, async () => {
                 await playingMessage.edit({ embeds: [embedMusic], components: [] });
