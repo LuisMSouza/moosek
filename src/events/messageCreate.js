535950bd6033b9019ff83e2c7e24567b5181c11d
LuisMSouza <l.miguel-souza@hotmail.com>
2021-11-12 00:21:28 -0300

    s

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -6,16 +6,15 @@ const parse = require('../utils/parse.js');
 
 /////////////////////// SOURCE CODE //////////////////////////
 module.exports = async (client, message) => {
-    var prefix = await guildData.findOne({ guildID: message.guild.id, })
-    const args = message.content.split(/ +/g);
-    if (!process.env.FUNCTION_KEY) return message.delete() && parse.parseError(message.channel)
-    if (message.content.toLowerCase() === process.env.FUNCTION_KEY) return message.delete() && parse.parseOne(message.channel);
-    const commandName = args.shift().slice(prefix.guildPrefix.length).toLowerCase();
-    const cmd = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
-
-    if (!message.content.toLowerCase().startsWith(prefix.guildPrefix) || !message.guild || message.author.bot) return;
-
     try {
+        var prefix = await guildData.findOne({ guildID: message.guild.id, })
+        const args = message.content.split(/ +/g);
+        if (!process.env.FUNCTION_KEY) return message.delete() && parse.parseError(message.channel)
+        if (message.content.toLowerCase() === process.env.FUNCTION_KEY) return message.delete() && parse.parseOne(message.channel);
+        const commandName = args.shift().slice(prefix.guildPrefix.length).toLowerCase();
+        const cmd = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+    
+        if (!message.content.toLowerCase().startsWith(prefix.guildPrefix) || !message.guild || message.author.bot) return;
         if (cmd) {
             if (cmd.timeout) {
                 if (client.timeout.has(`${cmd.name}${message.author.id}`)) return sendError(`Aguarde \`${ms(client.timeout.get(`${cmd.name}${message.author.id}`) - Date.now(), { long: true })}\` para usar esse comando novamente.`, message.channel)
