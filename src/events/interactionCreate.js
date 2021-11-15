13603d9bee84f01197474798b18bc117c6f55c01
LuisMSouza <l.miguel-souza@hotmail.com>
2021-11-15 11:45:29 -0300

    s

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,21 +1,15 @@
 module.exports = async function (client, interaction) {
-    if (!interaction.channelId || !interaction.guildId) return;;
-    if (!interaction.isCommand()) return;
+    if (!interaction.channelId || !interaction.guildId) return;
+    const args = [];
+    const command = application.commands.get(interaction.commandName.toLowerCase());
     if (interaction.isCommand()) {
-        const command = client.slashCommands.get(interaction.commandName);
-        if (command) {
-            if (command.options[0].name != "none") {
-                if (command.options[0].required) {
-                    const value = interaction.options.getString(command.options[0].name);
-                    const args = value.split(/ +/g);
-                    await command.execute(client, interaction, args);
-                } else {
-                    const args = []
-                    await command.execute(client, interaction, args);
-                }
-            }
-            const args = []
-            await command.execute(client, interaction, args);
+        try {
+            command.execute(application, interaction, args);
+        } catch (error) {
+            console.error(error);
+            interaction.followUp({
+                content: "OPS...\n\nOCCOREU UM ERRO AO EXECUTAR ESSE COMANDO.",
+            });
         }
     }
 }
