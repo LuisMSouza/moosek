6d79e276d220a1b527a455cf79dc3d7c8e9384f1
LuisMSouza <l.miguel-souza@hotmail.com>
2021-11-15 11:49:51 -0300

    s

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,10 +1,10 @@
 module.exports = async function (client, interaction) {
     if (!interaction.channelId || !interaction.guildId) return;
     const args = [];
-    const command = application.commands.get(interaction.commandName.toLowerCase());
+    const command = client.commands.get(interaction.commandName.toLowerCase());
     if (interaction.isCommand()) {
         try {
-            command.execute(application, interaction, args);
+            command.execute(client, interaction, args);
         } catch (error) {
             console.error(error);
             interaction.followUp({
