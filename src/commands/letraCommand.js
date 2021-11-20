102a964bd1593c8288f9aee40f2e2e5643c8dfff
LuisMSouza <l.miguel-souza@hotmail.com>
2021-11-20 19:59:58 -0300

    s

--- src/commands/letraCommand.js
+++ src/commands/letraCommand.js
@@ -50,7 +50,6 @@ module.exports = {
             }
         } else {
             try {
-                const search = await ytdl.getBasicInfo(`${main_entry}`)
                 await lyrics.search(`${main_entry}`).then(async r => {
                     await generateEmbeds(message, r, search.videoDetails.media.song, search.videoDetails.thumbnails[0].url, search.videoDetails.media.artist)
                     return msge.delete(msge);
