de5e1759b2a3193341bc5329282392cbf359ff5f
LuisMSouza <l.miguel-souza@hotmail.com>
2021-12-13 00:14:31 -0300

    s

--- src/commands/radioCommand.js
+++ src/commands/radioCommand.js
@@ -125,10 +125,7 @@ module.exports = {
                 )
             await message.reply("**Faça a escolha da rádio no menu abaixo:**")
             const msgEmb = await message.channel.send({
-                components: [row], embeds: [{
-                    description: "> **Faça a escolha da rádio clicando aqui**",
-                    color: "YELLOW"
-                }]
+                components: [row]
             });
             const filter = (i) => i.user.id === (message.member.user.id || message.user.id);
             const collector = message.channel.createMessageComponentCollector({ filter, max: 1, time: 300_000 });
